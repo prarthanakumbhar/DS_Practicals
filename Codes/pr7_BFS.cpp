3a8f3741246b577cb00a0141ea0e0ccf36a28c07
prarthanakumbhar <prarthanakumbhar2025.comp@mmcoe.edu.in>
2025-11-07 09:13:46 +0530

    Update pr7_BFS.cpp

--- Codes/pr7_BFS.cpp
+++ Codes/pr7_BFS.cpp
@@ -1,56 +1,59 @@
-//BFS
 #include <iostream>
 #define MAX 10
 using namespace std;
 
-int queue[MAX], front = -1, rear = -1, visited[MAX] = {0}, adj[MAX][MAX];
+int queueArr[MAX], front = -1, rear = -1;
+int visited[MAX] = {0};
+int adj[MAX][MAX];
 
 void enqueue(int v) {
-    if (rear == MAX - 1) printf("Queue Overflow\n");
+    if (rear == MAX - 1)
+        cout << "Queue Overflow\n";
     else {
-        if (front == -1) front = 0;
-        queue[++rear] = v;
+        if (front == -1)
+            front = 0;
+        queueArr[++rear] = v;
     }
 }
 
 int dequeue() {
-    return (front == -1 || front > rear) ? -1 : queue[front++];
+    if (front == -1 || front > rear)
+        return -1;
+    return queueArr[front++];
 }
 
 void bfs(int start, int n) {
     enqueue(start);
     visited[start] = 1;
-    printf("\nIndexing pages starting from: %d\n", start);
+    cout << "\nIndexing pages starting from: " << start << endl;
     while (front <= rear) {
         int v = dequeue();
-        printf("Indexed page: %d\n", v);
-        for (int i = 0; i < n; i++)
-            if (adj[v][i] && !visited[i]) {
+        cout << "Indexed page: " << v << endl;
+        for (int i = 0; i < n; i++) {
+            if (adj[v][i] == 1 && visited[i] == 0) {
                 enqueue(i);
                 visited[i] = 1;
             }
+        }
     }
 }
 
 int main() {
     int n, e, from, to, start;
-    printf("Enter number of web pages (max 10): ");
-    scanf("%d", &n);
-    printf("\nEnter number of hyperlinks: ");
-    scanf("%d", &e);
-    
+    cout << "Enter number of web pages (max 10): ";
+    cin >> n;
+    cout << "Enter number of hyperlinks: ";
+    cin >> e;
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             adj[i][j] = 0;
-    
-    printf("\nEnter links (from to):\n");
+    cout << "Enter links (from to):" << endl;
     for (int i = 0; i < e; i++) {
-        scanf("%d %d", &from, &to);
+        cin >> from >> to;
         adj[from][to] = 1;
     }
-    
-    printf("\nEnter seed/start page number: ");
-    scanf("%d", &start);
+    cout << "Enter seed/start page number: ";
+    cin >> start;
     bfs(start, n);
     return 0;
 }
